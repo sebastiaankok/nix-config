8a9ec2d2ef983a7356e42af0c113c7fe1e0000b1
Sebastiaan Kok <sebastiaankok@Sebastiaans-MacBook-Pro.local>
2024-01-30 13:47:06 +0100

    move user and homedir to other config

--- README.md
+++ README.md
@@ -7,4 +7,5 @@ This is my personal repo for home-manager configuration. Nix allows me to easily
 1) Install [nix](https://nixos.org/download)
 2) Install standalone [home-manager](https://nix-community.github.io/home-manager/index.xhtml#sec-install-standalone)
 3) Run `git clone https://github.com/sebastiaankok/nix-config.git ~/.config/home-manager`
+4) Run `./gen-config.sh` to setup user and homedir configuration
 3) Execute `home-manager switch` after setup, `alias up`
--- home.nix
+++ home.nix
@@ -7,12 +7,8 @@ let
 in
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   nixpkgs.config.allowUnfree = true;
-  home.username = "sebastiaankok";
-  home.homeDirectory = "/Users/sebastiaankok";
-  home.stateVersion = "23.11"; # Please read the comment before changing.
+  home.stateVersion = "23.11";
 
   home.packages = with pkgs; [
     python3
@@ -44,28 +40,14 @@ in
     restic
   ];
 
-
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
-
   home.sessionVariables = {
-    # EDITOR = "emacs";
   };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
   imports = [
+    ./configuration.nix
     nixvim.homeManagerModules.nixvim
     ./term
   ];
