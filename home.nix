8fa2fd188b34bb23b935c46ed4208b80837d2a95
Sebastiaan Kok <sebastiaankok@Sebastiaans-MacBook-Pro.local>
2024-01-30 01:46:04 +0100

    add kubectl

--- home.nix
+++ home.nix
@@ -25,6 +25,7 @@ in
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
+    pkgs.kubectl
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
