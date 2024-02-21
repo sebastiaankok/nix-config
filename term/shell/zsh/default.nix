10d17152fd337c70c01777b0a5759ed991818e72
Sebastiaan Kok <sebastiaan.kok@true.nl>
2024-02-21 19:37:32 +0100

    disable autosuggestion plugin since it's enabled in nix

--- term/shell/zsh/default.nix
+++ term/shell/zsh/default.nix
@@ -18,7 +18,6 @@
     zplug = {
       enable = true;
       plugins = [
-        { name = "zsh-users/zsh-autosuggestions"; } # Simple plugin installation
         { name = "romkatv/powerlevel10k"; tags = [ as:theme depth:1 ]; } # Installations with additional options. For the list of options, please refer to Zplug README.
         { name = "plugins/git"; tags = [ from:oh-my-zsh ]; }
         { name = "zsh-users/zsh-syntax-highlighting";}
