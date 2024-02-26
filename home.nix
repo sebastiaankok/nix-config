6c06f48c4b622cd76e289b933740302bac2c4ca5
Sebastiaan Kok <sebastiaan@linux.com>
2024-02-27 00:32:58 +0100

    refactor: stable/unstable packages to own file

--- home.nix
+++ home.nix
@@ -2,41 +2,12 @@
 let
   nixvim = import (builtins.fetchGit {
     url = "https://github.com/nix-community/nixvim";
-    #ref = "nixos-23.11";
   });
 in
 
 {
   nixpkgs.config.allowUnfree = true;
   home.stateVersion = "23.11";
-
-  home.packages = with pkgs; [
-    # languages
-    python3
-    python311Packages.pip
-    pre-commit
-    virtualenv
-    go
-
-    # system tools
-    yq
-    jq
-    coreutils-full
-    tree
-    nettools
-    vivid
-    diff-so-fancy
-    ripgrep
-    eza
-    lazygit
-    btop
-
-    # databases
-    postgresql
-  ];
-
-  home.sessionVariables = {
-  };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
@@ -51,9 +22,9 @@ in
   };
 
   imports = [
-    ./local.nix
     nixvim.homeManagerModules.nixvim
     ./term
-    ./unstable.nix
+    ./packages.nix
+    ./local.nix
   ];
 }
---
+++ packages.nix
@@ -0,0 +1,55 @@
+{ pkgs, config, ... }:
+let
+  unstable = import <nixpkgs-unstable> {
+    config = {
+      allowUnfree = true;
+    };
+  };
+in {
+
+  home.packages = with pkgs; [
+    # languages
+    python3
+    python311Packages.pip
+    pre-commit
+    virtualenv
+    go
+
+    # system tools
+    yq
+    jq
+    coreutils-full
+    tree
+    nettools
+    vivid
+    diff-so-fancy
+    ripgrep
+    eza
+    lazygit
+    btop
+
+    # databases
+    postgresql
+
+    # unstable
+    unstable.argocd
+    # k8s
+    unstable.k9s
+    unstable.kubectl
+    unstable.kubecolor
+    unstable.krew
+    unstable.kubernetes-helm
+    unstable.helm-docs
+    unstable.cilium-cli
+    unstable.teleport
+    unstable.stern
+    unstable.velero
+    unstable.restic
+
+    # Automation
+    unstable.vault
+    unstable.vault-medusa
+    unstable.bws
+
+  ];
+}
